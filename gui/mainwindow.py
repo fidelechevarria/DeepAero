38129027dba0570e17672d18a56637b771855bce
Fidel Echevarria <fidelpuerto92@gmail.com>
2019-10-23 19:24:43 +0200

    Moved gui to directory

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file 'gui/mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -15,15 +15,25 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.showMaximized()
         MainWindow.resize(800, 600)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuIdentify = QtWidgets.QMenu(self.menubar)
+        self.menuIdentify.setObjectName("menuIdentify")
+        self.menuSimulate = QtWidgets.QMenu(self.menubar)
+        self.menuSimulate.setObjectName("menuSimulate")
+        self.menuTrain = QtWidgets.QMenu(self.menubar)
+        self.menuTrain.setObjectName("menuTrain")
+        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuIdentify.menuAction())
+        self.menubar.addAction(self.menuSimulate.menuAction())
+        self.menubar.addAction(self.menuTrain.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -31,6 +41,9 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuIdentify.setTitle(_translate("MainWindow", "Identify"))
+        self.menuSimulate.setTitle(_translate("MainWindow", "Simulate"))
+        self.menuTrain.setTitle(_translate("MainWindow", "Train"))
 
 
 if __name__ == "__main__":
