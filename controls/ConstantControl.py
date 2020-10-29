81ea97e76051123242fb0e8d3d85534f58887c35
Fidel Echevarría Corrales <fidel.echevarria.corrales@gmail.com>
2020-10-29 23:46:31 +0100

    Deleted some obsolete files

--- controls/ConstantControl.py
+++
@@ -1,6 +0,0 @@
-from numba import jit
-import numpy as np
-
-@jit(nopython=True)
-def update(controls, states):
-    return np.array([0, 0, 0, 1], dtype=np.float32)
--- kinematics/ConstantKinematics.py
+++
@@ -1,6 +0,0 @@
-from numba import jit
-import numpy as np
-
-@jit(nopython=True)
-def update(states, forces, moments, dt):
-    return states
