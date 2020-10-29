9bd9f1fffa89951c8e9f3e8694f8a39724e7428b
Fidel Echevarría Corrales <fidel.echevarria.corrales@gmail.com>
2020-10-30 00:01:00 +0100

    Deleted comments in windows FG run script

--- FlightGear/runFGwindows.bat
+++ FlightGear/runFGwindows.bat
@@ -1,27 +1,3 @@
-
- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
- ::
- :: Copyright (c) 2016 UAV Navigation, S.L. All Rights Reserved.
- ::
- :: This computer software is owned by UAV Navigation, S.L. and is
- :: protected by U.S. copyright laws and other laws and by international
- :: treaties. The right to copy, distribute, or use this software in any
- :: form is absolutely restricted to UAV Navigation, S.L.
- ::
- :: UAV Navigation, S.L.
- :: Av Pirineos 7, B11
- :: SS de los Reyes, 28703 Madrid - SPAIN
- :: http://www.uavnavigation.com/
- ::
- ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-::
-:: This script file is used to invoke FlightGear together with the necessary
-:: options to run together with the VECTOR-HIL simulator platform.
-::
-:: Please modify the paths below according to your installation.
-::
-
 C:
 cd C:\Program Files\FlightGear 2018.3.2
 set FG_ROOT=C:\Program Files\FlightGear 2018.3.2\data
