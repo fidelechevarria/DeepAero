8b0cab3f400c69bc2adac039bb88df07d370d7cb
Fidel Echevarría Corrales <fidel.echevarria.corrales@gmail.com>
2020-11-05 18:56:22 +0100

    Added simple Dash app for trajectory visualization. Work in progress.

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+import optimize_cpp
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+df = optimize_cpp.optimize()
+
+fig = px.line_3d(df, x="north", y="east", z="down")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
