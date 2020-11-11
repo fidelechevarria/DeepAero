57aa31837c0aab46935c845cc04d5d97a82447ac
Fidel Echevarría Corrales <fidel.echevarria.corrales@gmail.com>
2020-11-11 19:21:36 +0100

    Added optimize button

--- app.py
+++ app.py
@@ -14,48 +14,70 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df_optim, df_real = optimize_cpp_cmaes.optimize()
-
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter3d(
-        x=df_optim['north'],
-        y=df_optim['east'],
-        z=-df_optim['down'],
-        mode='lines',
-        line={"color": 'blue'},
-        legendgroup=1,
-        hovertext="Optim",
-        showlegend=True,
-        name="Optimized"
-    )
-)
-fig.add_trace(
-    go.Scatter3d(
-        x=df_real['north'],
-        y=df_real['east'],
-        z=-df_real['down'],
-        mode='lines',
-        line={"color": 'red'},
-        legendgroup=1,
-        hovertext="Real",
-        showlegend=True,
-        name="Real"
-    )
-)
-
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H3(children='DeepAero'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Parameter identification for dynamic systems.
     '''),
 
+    html.Button(
+        'Optimize',
+        id='optimize-button',
+        n_clicks=0
+    ),
+
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='3Dtraj',
+        figure=go.Figure(),
+        style={
+            'width': '600px'
+        }
     )
 ])
+
+@app.callback(
+    dash.dependencies.Output('3Dtraj', 'figure'),
+    [dash.dependencies.Input('optimize-button', 'n_clicks')],
+    [dash.dependencies.State('3Dtraj', 'figure')])
+def update_output(n_clicks, current_fig):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if 'optimize-button' in changed_id:
+        df_optim, df_real = optimize_cpp_cmaes.optimize()
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scatter3d(
+                x=df_optim['north'],
+                y=df_optim['east'],
+                z=-df_optim['down'],
+                mode='lines',
+                line={"color": 'blue'},
+                legendgroup=1,
+                hovertext="Optim",
+                showlegend=True,
+                name="Optimized"
+            )
+        )
+        fig.add_trace(
+            go.Scatter3d(
+                x=df_real['north'],
+                y=df_real['east'],
+                z=-df_real['down'],
+                mode='lines',
+                line={"color": 'red'},
+                legendgroup=1,
+                hovertext="Real",
+                showlegend=True,
+                name="Real"
+            )
+        )
+        fig.update_layout(
+            margin=dict(l=25, r=25, t=25, b=25),
+            paper_bgcolor="White",
+        )
+    else:
+        fig = current_fig
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
