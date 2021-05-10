7d8da38060e3adde2fdc724a460a79e82aa6e20e
Fidel Echevarria Corrales <fidel.echevarria.corrales@gmail.com>
2021-05-11 00:47:36 +0200

    Added function for measuring evaluation time in Python wrapper

--- optimize_cpp_cmaes.py
+++ optimize_cpp_cmaes.py
@@ -77,6 +77,13 @@ class Optimizer():
         res = es.result
         return self.getTrajectory(res[0], trajFile), self.getTrajectory(defaultAero, trajFile)
 
+    def getEvaluationTimeInMicroseconds(self):
+        now = datetime.datetime.now()
+        self.model.evaluate(0.05, 0.01, 0.15, -0.4, 0, 0.19, 0, 0.4, 0.1205, 5.7, -0.0002, -0.33, 0.021,
+                           -0.79, 0.075, 0, -1.23, 0, -1.1, 0, -7.34, 0.21, -0.014, -0.11, -0.024, -0.265)
+        ellapsed = datetime.datetime.now() - now
+        return int(ellapsed.total_seconds() * 1e6) # microseconds
+
 if __name__ == "__main__":
     optimizer = Optimizer()
     optimizer.optimize("/home/fidel/repos/DeepAero/data.csv")
