2c69110de4302b05da730f883ca81239d908babe
Fidel Echevarria Corrales <fidel.echevarria.corrales@gmail.com>
2021-08-25 22:42:13 +0200

    Added calculation of metrics in optimize function

--- optimize_cpp_cmaes.py
+++ optimize_cpp_cmaes.py
@@ -1,3 +1,4 @@
+from scipy.spatial.distance import cdist
 import optimcore as optim
 import numpy as np
 import pandas as pd
@@ -85,17 +86,24 @@ class Optimizer():
         self.useLinVels = False
         self.numberOfSamplesToUse = -1
         es = cma.CMAEvolutionStrategy(x0, 0.2)
-        es.optimize(self.fitness)
+        # es.optimize(self.fitness)
+        while not es.stop():
+        # for _ in range(100):
+            solutions = es.ask()
+            es.tell(solutions, [self.fitness(s) for s in solutions])
+            es.disp()
+        es.result_pretty()
         res = es.result
         x0 = [element for element in res[0]]
-        pd.options.display.float_format = '{:,.5f}'.format
-        print(pd.DataFrame({'real': defaultAero, 'optim': x0}))
+        # pd.options.display.float_format = '{:,.5f}'.format
+        # print(pd.DataFrame({'real': defaultAero, 'optim': x0}))
         self.useLinVels = True
         es._set_x0(x0)
         # sigma0 = 0.01
         # es.__init__(x0, sigma0)
         # es.optimize(self.fitness)
         while not es.stop():
+        # for _ in range(100):
             solutions = es.ask()
             es.tell(solutions, [self.fitness(s) for s in solutions])
             es.disp()
@@ -104,7 +112,20 @@ class Optimizer():
         x0 = [element for element in res[0]]
         pd.options.display.float_format = '{:,.5f}'.format
         print(pd.DataFrame({'real': defaultAero, 'optim': x0}))
-        return self.getTrajectory(res[0]), self.getTrajectory(defaultAero)
+        ref_array = np.expand_dims(np.array(defaultAero), axis=1).T
+        sol_array = np.expand_dims(np.array(x0), axis=1).T
+        best_sol = res[0]
+        SD = cdist(ref_array, sol_array, metric='cityblock')[0][0] # Calculate Manhattan distance
+        BF = res[1]
+        ES = res[2]
+        FS = int(BF < 1e-2)
+        DS = int(SD < 5)
+        print('Manhattan distance: ' + str(SD))
+        print('Best fitness: ' + str(BF))
+        print('Number of evaluations: ' + str(ES))
+        print('Fitness success: ' + str(FS))
+        print('Distance success: ' + str(DS))
+        return self.getTrajectory(best_sol), self.getTrajectory(defaultAero)
 
     def getEvaluationTimeInMicroseconds(self):
         now = datetime.datetime.now()
